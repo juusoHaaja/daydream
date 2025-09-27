0de3c8de9bae19914bd5055383bfbfd52834f025
Zawaer <zawarudo123pro@gmail.com>
2025-09-27 21:26:42 +0300

    modified persons per pixel count to be more accurate

--- functional/world/main.gd
+++ functional/world/main.gd
@@ -11,9 +11,9 @@ static var instance;
 var kill_count = 0;
 
 func _ready() -> void:
-    instance = self
-    canvas.main = self
-    nuke_man.main = self
+	instance = self
+	canvas.main = self
+	nuke_man.main = self
 
 func update_kill_count():
-    killcount.update_count(kill_count)
+	killcount.update_count(kill_count)
