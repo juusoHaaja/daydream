86288420d0100e460d5bfbcc27352bac2d55902a
Zawaer <zawarudo123pro@gmail.com>
2025-09-27 23:26:32 +0300

    aaa

--- functional/ui/money_counter.gd
+++ functional/ui/money_counter.gd
@@ -8,18 +8,18 @@ var min_speed: float = 100.0  # minimal increment per second for small differenc
 var factor: float = 5.0      # how strongly step size scales with difference
 
 func _process(delta: float) -> void:
-    if current_displayed != target_count:
-        var diff = target_count - current_displayed
-        # step grows with difference
-        var step = max(min_speed, abs(diff) * factor) * delta
-        step = ceil(step)
-        # clamp so we don't overshoot
-        if abs(diff) <= step:
-            current_displayed = target_count
-        else:
-            current_displayed += step * sign(diff)
+	if current_displayed != target_count:
+		var diff = target_count - current_displayed
+		# step grows with difference
+		var step = max(min_speed, abs(diff) * factor) * delta
+		step = ceil(step)
+		# clamp so we don't overshoot
+		if abs(diff) <= step:
+			current_displayed = target_count
+		else:
+			current_displayed += step * sign(diff)
 
-        $Label.text = "Funding: %d billion $" % current_displayed
+		$Label.text = "Funding: %d billion $" % current_displayed
 
 func update_count(new_count: int) -> void:
-    target_count = new_count
+	target_count = new_count
